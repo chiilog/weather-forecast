83b8ca35184b96f6413cec3a37e4b6bc6b45c4fa
chiilog <purgexpurge@gmail.com>
2026-01-25 23:57:41 +0900

    feat: WeatherListItemの余白調整

--- src/components/WeatherListItem.tsx
+++ src/components/WeatherListItem.tsx
@@ -7,11 +7,17 @@ export const WeatherListItem = ({
   description,
 }: WeatherListItemProps) => {
   return (
-    <div className="bg-white border-b border-gray-200 p-2">
+    <div className="bg-white border-b border-gray-200 p-4">
       <p className="text-sm text-gray-600 mb-1">{dateTime}</p>
       <div className="flex items-center justify-start gap-4">
         <div className="flex flex-col items-center">
-          <img src={iconUrl} alt={description} className="h-20 w-20" width="80" height="80" />
+          <img
+            src={iconUrl}
+            alt={description}
+            className="h-20 w-20"
+            width="80"
+            height="80"
+          />
           <p className="text-sm text-gray-600 mt-1">{description}</p>
         </div>
         <p className="text-2xl font-bold">{temperature}℃</p>
